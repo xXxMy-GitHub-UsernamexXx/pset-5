f3763e68ff566e8667ffb15374b12e5f5385391b
Ethan Charles <ECharles1@ucvts.tec.nj.us>
2019-12-03 13:12:33 -0500

    initial commit

--- js/shapes.js
+++ js/shapes.js
@@ -1,25 +1,27 @@
 window.onload = function() {
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
-    document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
-    document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
-    document.getElementById("triangle-sample").onclick = drawTriangleStaff;
-    document.getElementById("smile-sample").onclick = drawFaceStaff;
-    document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
+        document.getElementById("hello-sample").onclick = sayHelloStaff;
+        document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
+        document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
+        document.getElementById("triangle-sample").onclick = drawTriangleStaff;
+        document.getElementById("smile-sample").onclick = drawFaceStaff;
+        document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-    document.getElementById("hello").onclick = sayHello;
-}
+// this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
+//
+// document.getElementById("some-id")   <-- you need to give each button a unique ID
+//                                          and access it in this manner
+//
+// onclick is an event that is fired when you click something (in our case, a button).
+// when we give onclick a value, we're telling JavaScript what to do when we click the button.
+// you should set onclick equal to your function names (i.e., sayHello).
+//
+// there are six event listeners being added for the staff solutions. you'll have an
+// equivalent set of six event listeners for your solutions. the first one is done for you.
+
+        document.getElementById("hello").onclick = sayHello;
+
+};
 
 /*
  * Exercise 1.
@@ -58,7 +60,7 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    // write your exercise 4 code here
+    // write your exercise 5 code here
 };
 
 /*
